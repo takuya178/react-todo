a35b7d0335735a9eeeb2a5375927aee836679dc6
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-09-24 19:23:29 +0900

    update: refactoring

--- src/components/templates/TodoTemplate/index.jsx
+++ src/components/templates/TodoTemplate/index.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+/**
+ * TodoTemplate
+ *
+ * @package components
+ */
+import { useState, useMemo } from "react";
 import { InputForm } from "../../atoms/InputForm";
 import { AddTodo } from "../../organisms/AddTodo";
 import { TodoList } from "../../organisms/TodoLlist";
-import { searchTodo } from "../../../utils/todoLogic";
 import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../../../constants/data.js";
 import styles from "./styles.module.css";
 
+/**
+ * TodoTemplate
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export const TodoTemplate = () => {
   /* todo list */
   const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
@@ -16,27 +25,24 @@ export const TodoTemplate = () => {
   /* 検索キーワード */
   const [searchKeyword, setSearchKeyword] = useState("");
   /* 表示用TodoList */
-  const [showTodoList, setShowTodoList] = useState(INIT_TODO_LIST);
+  const showTodoList = useMemo(() => {
+    return originTodoList.filter((todo) => {
+      // 検索キーワードに部分一致したTodoだけを一覧表示する
+      const regexp = new RegExp("^" + searchKeyword, "i");
+      return todo.title.match(regexp);
+    });
+    // useMemoの第二引数([originTodoList, searchKeyword])に依存して処理が実行される
+    // originTodoListとsearchKeywordの値が変更される度にfilterの検索処理が実行
+    // ただし結果が前回と同じならキャッシュを返却し処理は実行されない(無駄な処理を省いている)
+    // 詳しくはuseMemoを調べてください。
+  }, [originTodoList, searchKeyword]);
 
   /* actions */
   /**
    * addInputValueの変更処理
    * @param {*} e
    */
-  const onChangeAddInputValue = (e) => {
-    setAddInputValue(e.target.value);
-  };
-
-  /**
-   * 表示用Todoリスト更新処理
-   * @param {*} newTodoList
-   * @param {*} keyword
-   */
-  const updateShowTodoList = (newTodoList, keyword) => {
-    setShowTodoList(
-      keyword !== "" ? searchTodo(newTodoList, keyword) : newTodoList
-    );
-  };
+  const onChangeAddInputValue = (e) => setAddInputValue(e.target.value);
 
   /**
    * Todo新規登録処理
@@ -70,8 +76,6 @@ export const TodoTemplate = () => {
       ];
       setOriginTodoList(newTodoList);
 
-      updateShowTodoList(newTodoList, searchKeyword);
-
       // 採番IDを更新
       setUniqueId(nextUniqueId);
       // todo追加後、入力値をリセット
@@ -97,21 +101,14 @@ export const TodoTemplate = () => {
 
       // todoを削除したtodo listで更新
       setOriginTodoList(newTodoList);
-
-      updateShowTodoList(newTodoList, searchKeyword);
     }
   };
 
   /**
-   * Todo検索処理
+   * 検索キーワード更新処理
    * @param {*} e
    */
-  const handleSearchTodo = (e) => {
-    const keyword = e.target.value;
-    setSearchKeyword(keyword);
-
-    updateShowTodoList(originTodoList, keyword);
-  };
+  const handleChangeSearchKeyword = (e) => setSearchKeyword(e.target.value);
 
   return (
     <div className={styles.container}>
@@ -129,7 +126,7 @@ export const TodoTemplate = () => {
         <InputForm
           inputValue={searchKeyword}
           placeholder={"Search Keyword"}
-          handleChangeValue={handleSearchTodo}
+          handleChangeValue={handleChangeSearchKeyword}
         />
       </section>
       {/* Todoリスト一覧表示 */}
--- src/pages/todo.jsx
+++ src/pages/todo.jsx
@@ -1,3 +1,13 @@
+/**
+ * TodoPage
+ *
+ * @package pages
+ */
 import { TodoTemplate } from "../components/templates/TodoTemplate";
 
+/**
+ * TodoPage
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export const TodoPage = () => <TodoTemplate />;
